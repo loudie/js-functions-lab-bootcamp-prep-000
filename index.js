042d98d8a4ceabe31dce1314de9dbb24bc516bf9
Laura Diez <lauradiez12@gmail.com>
2018-08-09 04:30:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function happyHolidays() {
 }
 
 function happyHolidaysto(name) {
-  return "Happy holidays, ${name}!"
+  return `Happy holidays, ${name}!`
 }
 
 function happyHolidayto(holiday, name) {
